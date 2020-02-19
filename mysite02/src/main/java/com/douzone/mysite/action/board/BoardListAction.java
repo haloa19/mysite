55cbbbb3c51a589a5bc0ecf23f731007f9fbc262
haloa19 <hj199514@gmail.com>
2020-02-19 14:29:41 +0900

    pom.xml updated for jenkins build

--- mysite02/src/main/java/com/douzone/mysite/action/board/BoardListAction.java
+++ mysite02/src/main/java/com/douzone/mysite/action/board/BoardListAction.java
@@ -16,13 +16,12 @@ public class BoardListAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		// 게시판 처음 접속 시 무조건 1페이지 요청
-		// case1. 검색어가 있는 경우
-		// case2. 검색어가 없는 경우
+		// 0. 게시판 처음 접속 시 무조건 1페이지 요청
 		int maxGroup = 0;
-		String keyword = request.getParameter("kwd");		
-		String keyword2 = request.getParameter("keyword");
+		String keyword = request.getParameter("kwd"); // 검색어가 있는 경우 검색어를 처음 가져올때
+		String keyword2 = request.getParameter("keyword");  // 검색어가 있고 페이지 전환이 있는 경우 (kwd로 가져온 값은 null이 되기 때문에)
 	
+		// 1. 검색어 유무에 따른 group개수
 		if(keyword != null) { // 처음 검색해서 keyword가 존재하는 경우
 			maxGroup = new BoardRepository().findByKeyWordMaxGroup(keyword);
 		} else if(keyword2 != null) { // 검색 후 페이지 전환 했을 경우
@@ -32,46 +31,40 @@ public class BoardListAction implements Action {
 			maxGroup = new BoardRepository().maxGroupNum();
 		}
 		
-		// 페이지 정보 초기값
+		// 2. 페이지 정보 초기값 설정
 		int nowPage = 1;
 		int beginPage = 1;
 		double totalPage = Math.ceil((double)maxGroup/5);
 		int endPage = (int)totalPage; // < 1 > or <1, 2>		
+		List<BoardVo> list = null;	
 		
-		if(request.getParameter("page") != null) { // 페이지 이동이 이었을 경우
+		// 3. 페이지 정보에 따른 리스트 가져오기
+		// 3-1. 페이지 이동이 있는 경우
+		if(request.getParameter("page") != null) {
 			nowPage = Integer.parseInt(request.getParameter("page"));
 			if(nowPage != 1 && (nowPage - 1) % 3 == 0) { // 시작페이지 변경이 필요한 경우
 				beginPage = Integer.parseInt(request.getParameter("bpage")) + 1;
 			}
+			
+			// 3-1-1. 검색어가 있는 경우
+			if(keyword != null || keyword2 != null) {
+				list = new BoardRepository().findByKeyWord(Integer.parseInt(request.getParameter("page")), maxGroup, keyword);
+			} else { // 3-1-2. 검색어가 없는 경우
+				list = new BoardRepository().findAll(nowPage, maxGroup);
+			}			
+			
+		} else { // 3-2. 페이지 이동이 없는 경우
+			if(keyword == null) { // 3-2-1. 검색어가 없는 경우
+				list = new BoardRepository().findAll(1, maxGroup);
+			} else { // 3-2-2. 검색어가 있는 경우
+				list = new BoardRepository().findByKeyWord(1, maxGroup, keyword);
+			}	
+
 		}
 		
-		if(totalPage > 2) { // 보여지는 마지막 숫자 (토탈 페이지 아님)
+		if(totalPage > 2) { // 보여지는 마지막 숫자 (토탈 페이지 아님), totlapage가 1 or 2일 경우 totalpage == endpage
 			endPage = beginPage + 2;
 		}		
-
-		List<BoardVo> list = null;
-		
-		if(request.getParameter("page") == null) { // 페이지 이동이 없는 경우
-			if(keyword == null) { // 검색어가 없는 경우
-				list = new BoardRepository().findAll(1, maxGroup);
-			} else { // 검색어가 있는 경우
-				list = new BoardRepository().findByKeyWord(1, maxGroup, keyword);
-			}			
-		} else {
-			if(keyword == null) {
-				maxGroup = new BoardRepository().maxGroupNum();
-				list = new BoardRepository().findAll(nowPage, maxGroup);
-
-			} else {
-				maxGroup = new BoardRepository().findByKeyWordMaxGroup(keyword);
-				list = new BoardRepository().findByKeyWord(Integer.parseInt(request.getParameter("page")), maxGroup, keyword);
-			}
-			if(keyword2 != null) {
-				maxGroup = new BoardRepository().findByKeyWordMaxGroup(keyword);
-				list = new BoardRepository().findByKeyWord(Integer.parseInt(request.getParameter("page")), maxGroup, keyword);
-			}
-
-		}
 
 		request.setAttribute("list", list);
 		request.setAttribute("groupNum", maxGroup);
@@ -86,7 +79,6 @@ public class BoardListAction implements Action {
 		} else {
 			WebUtil.forward("/WEB-INF/views/board/list.jsp", request, response);
 		}
-
 	}
 
 }
