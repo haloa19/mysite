816f71d66bb4e13fad71cadbc603937af4ddbf6a
haloa19 <hj199514@gmail.com>
2020-03-20 17:40:18 +0900

    mysite03 validation 추가

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.douzone.mysite.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,12 +26,20 @@ public class UserController {
 	private UserService userService;
 
 	@RequestMapping(value="/join", method=RequestMethod.GET)
-	public String join() {
+	public String join(@ModelAttribute UserVo vo) {  // @ModelAttribute("userVo") UserVo vo class이름 앞 소문자가 디폴트 값
 		return "user/join";
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(UserVo vo) {
+	public String join(@Valid UserVo vo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+//			List<ObjectError> list = result.getAllErrors();
+//			for(ObjectError error : list) {
+//				System.out.println("error : " + error);
+//			}
+			model.addAllAttributes(result.getModel()); // error내용을 jsp로 전송
+			return "user/join";
+		}
 		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
--- mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import com.douzone.mysite.vo.GuestbookVo;
 
@@ -16,9 +17,8 @@ public class GuestbookRepository {
 	@Autowired
 	private SqlSession sqlSession;
 
-	public List<GuestbookVo> findAll() {
-		List<GuestbookVo> list = sqlSession.selectList("guestbook.findAll");
-		return list;
+	public List<GuestbookVo> findAll() {			
+		return sqlSession.selectList("guestbook.findAll");
 	}
 	
 	public int insert(GuestbookVo vo) {
